df9e48c7b11e65907cd93408da67b34ac98524b7
Tim Hinz <tim@chargeover.com>
2022-06-28 13:15:15 -0500

    feat(app): add telegram login component to app.vue

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -2,4 +2,23 @@
 
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <TelegramLogin
+    mode="callback"
+    telegram-login="ShoutItBot"
+    @callback="yourCallbackFunction" />
 </template>
+<script>
+import TelegramLogin from './components/TelegramLogin.vue';
+
+export default {
+  components: { TelegramLogin },
+  methods: {
+    yourCallbackFunction(user) {
+      // gets user as an input
+      // id, first_name, last_name, username,
+      // photo_url, auth_date and hash
+      console.log(user);
+    },
+  },
+};
+</script>
