d3a2b191e5876b5ed37e7973c239b9247c8107d1
Tim Hinz <tim@chargeover.com>
2022-06-28 10:24:01 -0500

    chore: add ESLint and prettier
    Includes tailwind, vue, and airbnb config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  settings: {
+    tailwindcss: {
+      config: 'packages/app/tailwind.config.js',
+    },
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:tailwindcss/recommended',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'airbnb',
+    'prettier',
+  ],
+  plugins: ['prettier', 'tailwindcss'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+  overrides: [
+    {
+      files: 'packages/api/**/*',
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
+};
