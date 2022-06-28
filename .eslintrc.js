0484400d9ccf36f76c24be33bcbfd4942ed9bd39
Tim Hinz <tim@chargeover.com>
2022-06-28 11:37:22 -0500

    chore: silence annoying react version warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
     tailwindcss: {
       config: 'packages/app/tailwind.config.js',
     },
+    // We don't actually use React, this only exists to silence a warning
+    // https://github.com/DRD4-7R/eslint-config-7r-building/issues/1#issuecomment-473031376
+    react: {
+      version: '99.99.99',
+    },
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
